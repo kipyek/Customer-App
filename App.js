efe337fc34b741d8b26bb1292521728e71e1fb5e
kipyek <deniskipyegon88@gmail.com>
2022-05-28 00:59:29 +0300

    database

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { StyleSheet, Text, View } from 'react-native'
 import RootNavigator from './src/Screens/Navigation'
 
 import { NavigationContainer } from '@react-navigation/native';
+import { Amplify } from 'aws-amplify'
+import { withAuthenticator } from 'aws-amplify-react-native';
+import config from './src/aws-exports'
 
+Amplify.configure(config)
 
-
-
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <RootNavigator />
@@ -15,3 +17,5 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+export default withAuthenticator(App);
